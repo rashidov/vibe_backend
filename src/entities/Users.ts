beca6cff5dd9c5041dc1ae9055cd5045b687f4da
arashidov <arashidov@smartdatatech.io>
2025-02-02 11:49:38 +0100

    fix(entities): rename methods & add return values

--- src/entities/Users.ts
+++ src/entities/Users.ts
@@ -1,4 +1,4 @@
-import {uid} from "uid";
+import { uid } from "uid";
 
 type User = {
   id: string
@@ -13,23 +13,32 @@ class UsersEntity {
     this.users = new Map()
   }
 
+  getUser(id: string) {
+    return this.users.get(id)
+  }
+
   addUser(login: string) {
     const id = uid(4)
     this.users.set(id, { id, login, socket_id: null })
+    return this.getUser(id)
   }
 
   removeUser(id: string) {
+    if (!this.users.has(id)) return false
     this.users.delete(id)
+    return true
   }
 
   addUsersSocketId(candidate: Omit<User, 'login'>) {
     if (!this.users.has(candidate.id)) return
     this.users.set(candidate.id, { ...this.users.get(candidate.id)!, socket_id: candidate.socket_id })
+    return this.getUser(candidate.id)
   }
 
   removeUsersSocketId(id: string) {
-    if (!this.users.has(id)) return
+    if (!this.users.has(id)) return false
     this.users.set(id, {...this.users.get(id)!, socket_id: null})
+    return true
   }
 }
 
