e13fce85593c16aec5d1268384c3c0ebbb06706d
arashidov <arashidov@smartdatatech.io>
2025-02-02 10:24:05 +0100

    feat(Users): add store

---
+++ src/store/Users.ts
@@ -0,0 +1,36 @@
+import {uid} from "uid";
+
+type User = {
+  id: string
+  login: string
+  socket_id: string | null
+}
+
+class Users {
+  users: Map<string, User>
+
+  constructor() {
+    this.users = new Map()
+  }
+
+  addUser(login: string) {
+    const id = uid(4)
+    this.users.set(id, { id, login, socket_id: null })
+  }
+
+  removeUser(id: string) {
+    this.users.delete(id)
+  }
+
+  addUsersSocketId(candidate: Omit<User, 'login'>) {
+    if (!this.users.get(candidate.id)) return
+    this.users.set(candidate.id, { ...this.users.get(candidate.id)!, socket_id: candidate.socket_id })
+  }
+
+  removeUsersSocketId(id: string) {
+    if (!this.users.get(id)) return
+    this.users.set(id, {...this.users.get(id)!, socket_id: null})
+  }
+}
+
+export default Users
