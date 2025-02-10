14988ed850ad856ace6e66f6d4fa75c6ea777d6f
arashidov <arashidov@smartdatatech.io>
2025-02-10 23:30:24 +0100

    fix(UserEndpoint): refactor

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -6,20 +6,13 @@ import { User } from '../types/user'
 class UserController {
   getAll(req: express.Request, res: express.Response) {
     const users = userService.getAllUsers()
-    return res.status(200).send({ users })
+    return res.status(200).send(users)
   }
   registration(req: express.Request<User>, res: express.Response) {
-    const candidate = req.body
-    const user = userService.create(candidate)
-    const room = roomService.createRoom({
-      maintainer: user.id,
-      name: 'General',
-    })
-    const rooms = userService.addRoomToUser({
-      user_id: user.id,
-      rooms: [room.id],
-    })
-    return res.status(200).send({ user, rooms })
+    if (!req.body?.login) return res.status(404).send({ message: 'Not found!' })
+    const candidate = req.body.login
+    const result = userService.createUser(candidate)
+    return res.status(200).send(result)
   }
 }
 
